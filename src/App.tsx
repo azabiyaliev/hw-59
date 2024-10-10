a58b46c91b718d6c4189642602f946b33c7a4eb5
azabiyaliev <aza.biyaliev@gmail.com>
2024-10-10 18:21:01 +0600

    Реализовано переключение между заданиями.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-// import MovieTask from './containers/MovieTask/MovieTask.tsx';
+import MovieTask from './containers/MovieTask/MovieTask.tsx';
 import JokesTask from './containers/JokesTask/JokesTask.tsx';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [page, setPage] = useState<boolean>(false);
+
   return (
     <>
-      <div className="container">
-        {/*<MovieTask/>*/}
-        <JokesTask/>
+      <div className="container text-center mt-5">
+        <button onClick={() => setPage(!page)} type='button' className='btn btn-outline-primary'>Change task</button>
+        {page ? <MovieTask/> : <JokesTask/>}
       </div>
     </>
   )
